2ee88740fc246f0fa1b23b84aafb534e0106fce2
levmn <levimncontato@gmail.com>
2025-06-06 00:09:50 -0300

    adicionando migrations

---
+++ SafeQuake.Infrastructure/Migrations/20250606030924_Inicial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SafeQuake.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Name = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Password = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Address = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EarthquakeEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Latitude = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    Longitude = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    Depth = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    Magnitude = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    Location = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EarthquakeEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EarthquakeEvents_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EarthquakeEvents_UserId",
+                table: "EarthquakeEvents",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EarthquakeEvents");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
